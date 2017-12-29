96df5431623388ab544ca198ba1c4a07e281b50b
masanarih0ri <masanarihoridevelop@gmail.com>
2017-12-29 17:44:53 +0900

    簡易バリデーションフォームを作成

---
+++ docs/js/validation.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.required').on('keydown keyup keypress change focus blur', function(){
+    if($(this).val() == ''){
+      $(this).css({backgroundColor: '#ffcccc'});
+    } else {
+      $(this).css({backgroundColor: '#fff'});
+    }
+  }).change();
+});
