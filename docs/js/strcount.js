b68e288579bec80e253a61b76c7f3c785e2195ce
masanarih0ri <masanarihoridevelop@gmail.com>
2018-01-07 13:03:35 +0900

    文字数カウントのバリデーションを追加

---
+++ docs/js/strcount.js
@@ -0,0 +1,8 @@
+$(function(){
+  var countThis = $('.inputCount');
+
+  countThis.find('textarea').on('keydown keyup keypress change', function(){
+    var thisValueLength = $(this).val().length;
+    countThis.find('.count').text(thisValueLength);
+  });
+});
